aa63d986a018da33e825ca10a790ce91fc8ec8c0
2Ruk <leerukdev@gmail.com>
2022-05-14 19:53:21 +0900

    feat(DB): MongoDB 연결
    MongoDB 연결입니다.
    .env 에 필요한 환경변수를 넣어주시면 됩니다.

--- backend/libs/my-library/src/my-library.module.ts
+++ backend/libs/my-library/src/my-library.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
 import { MyLibraryService } from './my-library.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+require('dotenv').config();
+
+const uri = process.env.uri;
 
 @Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      connectionName: 'Survey',
+      useFactory: () => ({
+        uri: uri,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [MyLibraryService],
   exports: [MyLibraryService],
 })
