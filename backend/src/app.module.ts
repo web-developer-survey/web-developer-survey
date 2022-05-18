b29b681b232f2935f0bb353feb07bd8650692261
2Ruk <leerukdev@gmail.com>
2022-05-19 00:35:21 +0900

    refactor(question ): remove

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import {MyLibraryModule} from "@app/my-library";
-import {QuestionModule} from "@app/api/question/question.module";
 import {SurveyModule} from "@app/api/survey/survey.module";
 import {AppController} from "@app/api/app.controller";
 import {AppService} from "@app/api/app.service";
 
 @Module({
-  imports: [SurveyModule, QuestionModule,MyLibraryModule],
+  imports: [SurveyModule,MyLibraryModule],
   controllers: [AppController],
   providers: [AppService],
 })
