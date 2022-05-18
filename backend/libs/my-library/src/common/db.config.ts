768bbcd001a6dc0e8d62009d8bbcfd2d95b5e782
2Ruk <leerukdev@gmail.com>
2022-05-19 00:30:53 +0900

    refactor(Share-library): DB Module
    DB 관련 설정을 위한
    1. env 설정
    2. db.config.ts
    3. createFeature 함수 생성
    생성

--- backend/libs/my-library/src/common/db.config.ts
+++ backend/libs/my-library/src/common/db.config.ts
@@ -1,17 +1,24 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DynamicModule, Type } from '@nestjs/common';
+import { AsyncModelFactory, MongooseModule, SchemaFactory } from '@nestjs/mongoose';
 
 require('dotenv').config({
   path: '../.env',
 });
-export const uri: string = process.env.uri;
 
-export const dbInfo = {
-  SURVEY: 'SURVEY',
-};
+export const uri: string = process.env.uri;
+export const enum DB_NAME {
+  DevSurvey = 'WebDevSurvey',
+}
+
+export const enum CollectionInfo {
+  SURVEY = 'Survey',
+  ANSWER = 'ANSWER',
+}
 
 export const dbOption = {
   imports: [ConfigModule],
-  connectionName: dbInfo.SURVEY,
+  connectionName: DB_NAME.DevSurvey,
   useFactory: () => ({
     uri: uri,
     useNewUrlParser: true,
@@ -19,3 +26,15 @@ export const dbOption = {
   }),
   inject: [ConfigService],
 };
+
+export function createFeature<TClass extends any>(target: Type<TClass>): DynamicModule {
+  const targetSchema = SchemaFactory.createForClass(target);
+  const targetFactory: AsyncModelFactory = {
+    name: target.name,
+    useFactory: () => {
+      return targetSchema;
+    },
+  };
+  const modelFactory: AsyncModelFactory[] = [targetFactory];
+  return MongooseModule.forFeatureAsync(modelFactory, DB_NAME.DevSurvey);
+}
--- backend/src/survey/survey.module.ts
+++ backend/src/survey/survey.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SurveyService } from './survey.service';
 import { SurveyController } from './survey.controller';
+import { surveyFeature } from '@app/my-library/models/survey/entities/survey.schema';
 
 @Module({
+  imports: [surveyFeature],
   controllers: [SurveyController],
   providers: [SurveyService],
+  exports: [SurveyService],
 })
 export class SurveyModule {}
