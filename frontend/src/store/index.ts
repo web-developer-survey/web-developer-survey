d002c2fd1b5e3049b3c208f11631cfa2275aa09f
2Ruk <leerukdev@gmail.com>
2022-06-04 00:18:22 +0900

    feat(store ): init

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,26 +1,27 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
-export const state:{balanceAnswersIdx:number[]} = {
-  balanceAnswersIdx: []
-
-}
+export const state: { balanceAnswersIdx: number[] } = {
+  balanceAnswersIdx: [],
+};
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
   state,
   getters: {
-    balanceAnswersIdx: (state) => state.balanceAnswersIdx
+    balanceAnswersIdx: (state) => state.balanceAnswersIdx,
   },
   actions: {
-    balanceVote({commit},questionIdx){
-      commit('')
-    }
+    balanceVote({ commit }, questionIdx) {
+      commit('balanceAnswersIdx', questionIdx);
+    },
   },
   mutations: {
-    balanceVote(state, questionIdx){
-      state.balanceAnswersIdx.push(questionIdx)
-    }
+    balanceVote(state, questionIdx) {
+      state.balanceAnswersIdx.push(questionIdx);
+    },
   },
   modules: {},
 });
