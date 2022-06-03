35ffe0f7a694cd2776324f88a2a7cc220a80a5cb
2Ruk <leerukdev@gmail.com>
2022-06-03 09:48:46 +0900

    feat(store ): 세팅중

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -2,11 +2,25 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
+export const state:{balanceAnswersIdx:number[]} = {
+  balanceAnswersIdx: []
+
+}
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state,
+  getters: {
+    balanceAnswersIdx: (state) => state.balanceAnswersIdx
+  },
+  actions: {
+    balanceVote({commit},questionIdx){
+      commit('')
+    }
+  },
+  mutations: {
+    balanceVote(state, questionIdx){
+      state.balanceAnswersIdx.push(questionIdx)
+    }
+  },
   modules: {},
 });
