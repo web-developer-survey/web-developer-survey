26cc3899e83cb26cc529bf59c94631e900063bb4
2Ruk <leerukdev@gmail.com>
2022-06-13 22:37:37 +0900

    feat(App)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,16 @@
 <template>
   <v-app>
-    <!--    <Header />-->
-
     <router-view />
-    <!--    </MainLayout>-->
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import Header from '@/components/survey/Header.vue';
-import Component from 'vue-class-component';
-import SurveyView from '@/views/SurveyView.vue';
-import MainLayout from '@/components/survey/MainLayout.vue';
+import { Component, Vue } from 'vue-property-decorator';
+import SurveyView from '@/vi ews/SurveyView.vue';
 
 @Component({
   components: {
-    QuestionView: SurveyView,
-    MainLayout,
-    Header,
+    SurveyView,
   },
 })
 export default class App extends Vue {
