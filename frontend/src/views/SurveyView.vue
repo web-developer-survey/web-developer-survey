58732dfbc118224b5d9999898ee9bc61b961f89f
2Ruk <leerukdev@gmail.com>
2022-06-14 21:03:58 +0900

    feat(Survey) : survey 구현 중 ( step 단위 별 )

--- frontend/src/views/SurveyView.vue
+++ frontend/src/views/SurveyView.vue
@@ -1,92 +1,82 @@
 <template>
   <v-main class="grey lighten-3">
     <v-container>
-      <v-sheet class="mx-auto" min-height="98vh" rounded="lg" max-width="800px">
+      <v-sheet class="mx-auto" max-width="800px" min-height="98vh" rounded="lg">
         <v-row>
           <v-col cols="12">
-
-            <v-subheader><v-icon color="info" v-text="'mdi-format-list-checkbox'"></v-icon>웹 개발자 설문조사</v-subheader>
-            <v-progress-linear
-                class="px-15"
-                value="10"
-                color="primary"
-                height="25"
-            >
-              <strong>10%</strong>
+            <v-subheader>
+              <v-icon color="info" v-text="'mdi-format-list-checkbox'"></v-icon>
+              웹 개발자 설문조사
+            </v-subheader>
+            <v-progress-linear :value="gage" class="px-15" color="primary" height="25">
+              <strong>{{ gage }}%</strong>
             </v-progress-linear>
             <!--            <v-btn @click="senDClick">테이터 생성</v-btn>-->
           </v-col>
-          <v-col cols="12">
+          <v-col :key="step" cols="12">
             <v-divider />
-            <v-container fluid class="px-5">
-              <v-card class="mb-5" elevation="0" outlined v-for="(item, idx) in surveyQuestions" :key="item.title">
-                <v-card-title class="border-left-2196f3  font-weight-medium text-body-1 text-sm-subtitle-1">
-                  <v-icon color="info" >mdi-alpha-q-box</v-icon> {{ item.title }}
+            <v-container class="px-5" fluid>
+              <v-card v-for="(item, idx) in stepQuestion" :key="item.title" class="mb-5" elevation="2" outlined>
+                <v-card-title class="border-left-2196f3 font-weight-medium text-body-1 text-sm-subtitle-1">
+                  <v-icon color="info">mdi-alpha-q-box</v-icon>
+                  {{ item.title }}
                 </v-card-title>
-                <v-divider  class="text-left"/>
+                <v-divider class="text-left" />
+                <v-alert v-if="item.desc" class="pa-5" text tile>
+                  <v-card-text v-html="item.desc"></v-card-text>
+                </v-alert>
                 <v-card-text>
-
-                  <template v-if="item.type==='COMPLETE' ">
-
+                  <template v-if="item.type === 'COMPLETE'">
                     <!--후보 1 -->
-                      <v-radio-group class="text-center" row>
-                        <v-radio
-                            class=" col-md-6 ma-0 pa-0 col-sm-12"
-                            v-for="(labelInfo,labelIdx) in item.viewInfo" :key="item.seq + '-' + labelInfo.value"
-                          :value="labelIdx"
-                      ><template #label>
-                          {{labelInfo.label}}
+                    <v-radio-group class="text-center" row>
+                      <v-radio
+                        v-for="(labelInfo, labelIdx) in item.viewInfo"
+                        :key="item.seq + '-' + labelInfo.value"
+                        :value="labelIdx"
+                        class="col-md-6 ma-0 pa-0 col-sm-12"
+                      >
+                        <template #label>
+                          {{ labelInfo.label }}
                         </template>
-                        </v-radio>
-                      </v-radio-group>
-
-                    <!-- 후보 2 -->
-<!--                    <v-row>-->
-<!--                      <v-col md="6" sm="12" v-for="(labelInfo,labelIdx) in item.viewInfo" :key="item.seq + '-' + labelInfo.value">-->
-<!--                        <v-btn-->
-<!--                            dark-->
-<!--                            block-->
-<!--                          :color="!labelIdx?'teal darker-1':'grey  darken-1' "-->
-<!--                        >{{labelInfo.label}}</v-btn>-->
-<!--                      </v-col>-->
-<!--                    </v-row>-->
-
-
+                      </v-radio>
+                    </v-radio-group>
 
                   </template>
 
-
-                  <template v-if="item.type==='RADIO' ">
-                  <v-radio-group   >
-                    <v-radio
-                      v-for="labelInfo in item.viewInfo"
-                      :key="item.seq + '-' + labelInfo.value"
-                      :value="labelInfo.value"
-                      ripple
-                    >
-                      <template #label>
-                        <v-row>
-
-                          <v-col cols="6" v-if="labelInfo.addText">
-                            <v-text-field class="ma-0"  label="기타"></v-text-field>
-                          </v-col>
-                          <v-col cols="12"  v-else>
-                            <label class="text-body-2 text-sm-body-1">{{labelInfo.label}}</label>
-                          </v-col>
-                        </v-row>
-                      </template>
-
-                    </v-radio>
-                  </v-radio-group>
+                  <template v-if="item.type === 'RADIO'">
+                    <v-radio-group>
+                      <v-radio
+                        v-for="labelInfo in item.viewInfo"
+                        :key="item.seq + '-' + labelInfo.value"
+                        :value="labelInfo.value"
+                        ripple
+                      >
+                        <template #label>
+                          <v-row>
+                            <v-col v-if="labelInfo.addText" cols="6">
+                              <v-text-field class="ma-0" label="기타"></v-text-field>
+                            </v-col>
+                            <v-col v-else cols="12">
+                              <label class="text-body-2 text-sm-body-1">{{ labelInfo.label }}</label>
+                            </v-col>
+                          </v-row>
+                        </template>
+                      </v-radio>
+                    </v-radio-group>
                   </template>
                 </v-card-text>
 
                 <v-divider />
               </v-card>
-              <v-card>
-                <v-btn>Prev(이전)</v-btn>
-                <v-btn>Next(다음)</v-btn>
-              </v-card>
+              <v-sheet height="200px" />
+
+              <v-sheet class="text-center">
+                <v-row align-content="center" justify="center">
+                  <v-col cols="6">
+                    <v-btn block color="indigo darker-3" dark large @click="vote">Next(다음)</v-btn>
+                  </v-col>
+                </v-row>
+              </v-sheet>
             </v-container>
           </v-col>
         </v-row>
@@ -107,27 +97,31 @@ export default class SurveyView extends Vue {
   private message: string = '';
   private loading: boolean = false;
   private row: number = 0;
-
+  private step: number = 1;
   private viewInfo = viewInfo;
   private surveyQuestions: Survey.Question[] = sampleQuestion;
 
-  get step4(): Survey.Question[] {
-    return this.surveyQuestions.filter((question) => question.step === 1);
+  get stepQuestion(): Survey.Question[] {
+    return this.surveyQuestions.filter((question) => question.step === this.step);
   }
 
-  created() {
-    const routerName = this.$route.name;
+  get gage(): number {
+    return this.step * 10;
   }
 
   // async senDClick() {
   //   const { data } = await this.axios.post('/survey/question', this.surveyQuestions);
   // }
 
+  created() {
+    const routerName = this.$route.name;
+  }
+
   async mounted() {
     // await this.getLoadTest();
     this.surveyQuestions = this.surveyQuestions.map((item1, idx) => {
       const findViewInfo = viewInfo.filter((v) => v.questionSEQ === item1.seq);
-    //
+      //
       const custom = findViewInfo.map((item) => {
         const { questionSEQ, isAllDisable, value, label, addText } = item;
 
@@ -147,6 +141,12 @@ export default class SurveyView extends Vue {
   }
 
   async getLoadTest() {}
+
+  vote() {
+    if (this.step >= 10) return;
+    this.$vuetify.goTo(0, { duration: 300 });
+    this.step++;
+  }
 
   reserve() {
     console.log('버튼 클릭');
