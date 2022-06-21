9e3975e63e5b4b1e349bd665790d04f0bcccf477
2Ruk <leerukdev@gmail.com>
2022-06-21 17:44:27 +0900

    feat(ttl ): set

--- backend/libs/my-library/src/my-library.module.ts
+++ backend/libs/my-library/src/my-library.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { MyLibraryService } from './my-library.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { dbSurveyOption } from '@app/my-library/common/db.config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 @Module({
-  imports: [MongooseModule.forRootAsync(dbSurveyOption)],
+  imports: [
+    MongooseModule.forRootAsync(dbSurveyOption),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        ttl: 3, // 보호되는 애플리케이션 경로에 대한 , TTL (Time to Live)
+        limit: 25, //최대 요청 수에 대한 전역 옵션
+      }),
+    }),
+  ],
   providers: [MyLibraryService],
   exports: [MyLibraryService],
 })
