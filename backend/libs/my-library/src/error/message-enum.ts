23a1daeb60595415227d8f06ed9365c678e1d35b
2Ruk <leerukdev@gmail.com>
2022-06-21 18:19:30 +0900

    feat(error): custom

---
+++ backend/libs/my-library/src/error/message-enum.ts
@@ -0,0 +1,3 @@
+export const enum ErrorMessage{
+  balanceFind = '밸런스 게임 문항 찾기 중 오류가 발생하였습니다.'
+}
--- backend/src/balance/balance-question/balance-question.service.ts
+++ backend/src/balance/balance-question/balance-question.service.ts
@@ -2,17 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { CreateBalanceQuestionDto } from './dto/create-balance-question.dto';
 import { UpdateBalanceQuestionDto } from './dto/update-balance-question.dto';
 import { BalanceQuestionRepository } from '@app/api/balance/balance-question/balance.question.repository';
+import {ErrorService} from "@app/my-library/error/error.service";
+import {ErrorMessage} from "@app/my-library/error/message-enum";
 
 @Injectable()
 export class BalanceQuestionService {
-  constructor(private balanceQuestionRepo: BalanceQuestionRepository) {}
+  constructor(private balanceQuestionRepo: BalanceQuestionRepository, private readonly errorService: ErrorService) {}
 
   create(createBalanceQuestionDto: CreateBalanceQuestionDto) {
     return 'This action adds a new balanceQuestion';
   }
 
   async findQuestion(balanceAnswers: number[]) {
-    return await this.balanceQuestionRepo.findAll(balanceAnswers);
+    try{
+      return await this.balanceQuestionRepo.findAll(balanceAnswers);
+    }catch (e) {
+      this.errorService.setCustomMessage(e, ErrorMessage.balanceFind);
+    }
   }
 
   async findAll() {
