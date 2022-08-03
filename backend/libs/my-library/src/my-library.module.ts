9b2670459acc93375d8b68d7957a736ef3d0e878
2Ruk <leerukdev@gmail.com>
2022-08-03 17:46:44 +0900

    chore: log

--- backend/libs/my-library/src/my-library.module.ts
+++ backend/libs/my-library/src/my-library.module.ts
@@ -4,8 +4,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { dbSurveyOption } from '@app/my-library/common/db.config';
 import { ErrorModule } from '@app/my-library/error/error.module';
 
-console.log(dbSurveyOption);
-
 @Module({
   imports: [MongooseModule.forRootAsync(dbSurveyOption), ErrorModule],
   providers: [MyLibraryService],
